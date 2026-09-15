28b7d2a6117a47cc34728bb0607acac9253dac6f
sabrinagiometto <sabrina.giometto@gmail.com>
2026-09-15 16:20:58 +0200

    Changed according to the latest version of the protocol (discont and switch 12 e 24 + restarter)

--- p_steps/05_T4_20_Create_D5_cohort_characteristics.R
+++ p_steps/05_T4_20_Create_D5_cohort_characteristics.R
@@ -6,6 +6,9 @@
 
 # v 1.2 10 Set 2026 Directories fixed
 
+# v 1.3 15 Set 2026 Changes according to the latest version of the protocol 
+#                   (discont e switch 12 e 24 + restarter)
+
 #########################################
 
 if (TEST){
@@ -31,24 +34,42 @@ if (TEST){
 # 
 # }
 
+for (i in drug_names) {
+  
+  data <- readRDS(file = paste0(thisdirinput, "/D3_coorte_con_caratterizzazione_", i, ".rds"))
+  
+  data <- as.data.table(data)
+  
+  assign(paste0("D3_coorte_con_caratterizzazione_", i), data)
+  
+}
 
-data <- readRDS(file = paste0(thisdirinput, "/D3_coorte_con_caratterizzazione.rds"))
-data <- as.data.table(data)
+# 
+# data <- readRDS(file = paste0(thisdirinput, "/D3_coorte_con_caratterizzazione.rds"))
+# data <- as.data.table(data)
 
-# remove prevalent users
-data <- data[user_type!="prev" ,]
+
 
 
 for (j in drug_names) {
+  
+  # remove prevalent users
+  data <- get(paste0("D3_coorte_con_caratterizzazione_", j))[user_type!="prev" ,]
+  
+  # create period_first
+  data[, period_first:=fcase(year_first %in% c(2016:2019), "2016-2019",
+                             year_first %in% c(2020:2022), "2020-2022",
+                             year_first %in% c(2023:2025), "2023-2025",
+                             default = NA)]
 
   # Create D5 with sociodemographic characteristics
-  D5_nocov <- data[drug==j, .(
+  D5_nocov <- data[, .(
                 N          = .N,
                 age_median = median(age),
                 age_q1 = quantile(age, probs = 0.25),
                 age_q3 = quantile(age, probs = 0.75),
-                genere_F_N = sum(genere=="F"),
-                genere_F_p = round(sum(genere=="F")/.N,3)*100,
+                genere_F_N = sum(gender=="F"),
+                genere_F_p = round(sum(gender=="F")/.N,3)*100,
                 conc_treat_median = median(conc_treat),
                 conc_treat_q1 = quantile(conc_treat, probs = 0.25),
                 conc_treat_q3 = quantile(conc_treat, probs = 0.75)),
@@ -58,14 +79,16 @@ for (j in drug_names) {
   covariates_binary <- c("iperten", "cardioisc", "infart", "arit", "angi",
                          "ictus", "tia", "scompcard", "dislip", "diab", "renal",
                          "cortic", "antiang", "antitromb", "ipolip", "antidiab",
-                         "bifosf", "switch_apalu", "switch_enzalu",
-                         "switch_darolu", "switch_other_oncol", "conc_treat")
+                         "bifosf", "switch_apalu_12", "switch_enzalu_12", "switch_darolu_12", 
+                         "switch_other_oncol_12", "discont_12", "discont_24",
+                         "switch_apalu_24", "switch_enzalu_24", "switch_darolu_24", 
+                         "switch_other_oncol_24", "restarter")
 
   D5_cov <- NULL
 
   for (i in covariates_binary) {
 
-    tmp <- data[drug==j, .(
+    tmp <- data[, .(
                 N = .N,
                 tmp_N = sum(get(i)==1),
                 tmp_p = round(sum(get(i)==1)/.N,3)*100),
